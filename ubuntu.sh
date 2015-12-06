d1b4f1dba0106d3238206fc1c0a0fe50bb158132
70_10 <chiclown.dev@gmail.com>
2015-12-07 00:47:44 +0900

    initial at ubuntu

---
+++ ubuntu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if ! [ `which ansible` ]; then
+  sudo apt-get install software-properties-common -y
+  sudo apt-add-repository ppa:ansible/ansible -y
+  sudo apt-get update
+  sudo apt-get install ansible -y
+fi
+
+ansible-playbook -i playbook/hosts/ubuntu -t ubuntu playbook/setup.yml
