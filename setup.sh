e37080b94f893924c7816ac3ddbabafc5cd61fc4
70-10 <chiclown.dev@gmail.com>
2015-12-11 00:28:29 +0900

    [mod] install brew & ansible

--- setup.sh
+++ setup.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
+if ! [ `which brew` ]; then
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+if ! [ `which ansible` ]; then
+  brew install ansible
+fi
+
 ansible-playbook -i playbook/hosts/local -t mac playbook/setup.yml
