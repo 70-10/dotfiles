39986c1510175c365dc3443fa581b2191dbe697b
70_10 <chiclown.dev@gmail.com>
2016-02-06 16:11:03 +0900

    [mod] check zplug module install

--- playbook/roles/zplug/files/zplug.sh
+++ playbook/roles/zplug/files/zplug.sh
@@ -4,12 +4,12 @@ zplug "mollifier/cd-gitroot"
 zplug "zsh-users/zsh-completions"
 
 # Install plugins if there are plugins that have not been installed
-# if ! zplug check --verbose; then
-#     printf "Install? [y/N]: "
-#     if read -q; then
-#         echo; zplug install
-#     fi
-# fi
+if ! zplug check --verbose; then
+    printf "Install? [y/N]: "
+    if read -q; then
+        echo; zplug install
+    fi
+fi
 
 # Then, source plugins and add commands to $PATH
 zplug load
