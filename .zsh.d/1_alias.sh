c65c796b40c0b03f9b01e88e2cd684656096a4f9
70_10 <chiclown.dev@gmail.com>
2016-02-17 01:49:24 +0900

    [add] translate-shell at homebrew

--- .zsh.d/1_alias.sh
+++ .zsh.d/1_alias.sh
@@ -2,6 +2,9 @@ alias history='history -i'
 alias lap='la | peco'
 alias resource='source ~/.zshrc'
 
+alias tj='trans ja:en'
+alias te='trans en:ja'
+
 # Go
 alias goget='go get'
 alias gorn='go run'
