d7702883eba04bf506e4e57317b536b1098d2033
70_10 <chiclown.dev@gmail.com>
2016-02-17 10:15:21 +0900

    [mod] update alias

--- .zsh.d/1_alias.sh
+++ .zsh.d/1_alias.sh
@@ -2,8 +2,8 @@ alias history='history -i'
 alias lap='la | peco'
 alias resource='source ~/.zshrc'
 
-alias tj='trans ja:en'
-alias te='trans en:ja'
+alias tje='trans ja:en'
+alias tej='trans en:ja'
 
 # Go
 alias goget='go get'
