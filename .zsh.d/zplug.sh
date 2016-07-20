279e567331e7407fbee154a9e5e6bffe2f190a75
70_10 <7010.dev@gmail.com>
2016-07-20 14:02:46 +0900

    nice zsh-syntax-highlighting

--- .zsh.d/zplug.sh
+++ .zsh.d/zplug.sh
@@ -2,7 +2,7 @@ source ~/.zplug/zplug
 
 zplug "mollifier/cd-gitroot", as:command
 zplug "zsh-users/zsh-completions"
-zplug "zsh-users/zsh-syntax-highlighting"
+zplug "zsh-users/zsh-syntax-highlighting", nice:10
 zplug "zsh-users/zsh-history-substring-search"
 zplug "themes/minimal", from:oh-my-zsh
 zplug "supercrabtree/k"
