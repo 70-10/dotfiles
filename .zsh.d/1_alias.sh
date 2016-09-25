81b73b26af437b01da47a959a9b2d989149e6da9
70_10 <7010.dev@gmail.com>
2016-09-25 13:01:24 +0900

    [mod] alias

--- .zsh.d/1_alias.sh
+++ .zsh.d/1_alias.sh
@@ -1,13 +1,10 @@
 alias cdu='cd-gitroot'
-alias history='history -i'
 alias ka='k -a'
-alias lap='la | peco'
 alias mkdirp='mkdir -p'
 alias resource='source ~/.zshrc'
 
 alias trje='trans ja:en'
 alias trej='trans en:ja'
 
-# Go
-alias goget='go get'
-alias gorn='go run'
+# docker
+alias d-rmall='docker rm -v $(docker ps -aq -f status=exited)'
