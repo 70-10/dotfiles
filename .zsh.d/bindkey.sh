9a1aa0c7aeb2672c604eea6da70fdc376c6efe78
70_10 <7010.dev@gmail.com>
2016-11-19 17:23:14 +0900

    [add] peco-aws-credentials

--- .zsh.d/bindkey.sh
+++ .zsh.d/bindkey.sh
@@ -1,9 +1,11 @@
-autoload -Uz peco-src peco-history
+autoload -Uz peco-src peco-history peco-aws-credentials
 zle -N peco-src
 zle -N peco-history
+zle -N peco-aws-credentials
 
 bindkey ";5C" forward-word
 bindkey ";5D" backward-word
 bindkey "\e[3~" delete-char
 bindkey '^]' peco-src
 bindkey '^r' peco-history
+bindkey '^w' peco-aws-credentials
