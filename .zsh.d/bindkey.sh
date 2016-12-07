8aabfe839cc3ce174bbdbdae12a2449a23e9ac84
70_10 <7010.dev@gmail.com>
2016-12-07 12:44:07 +0900

    [add] peco-z-search

--- .zsh.d/bindkey.sh
+++ .zsh.d/bindkey.sh
@@ -1,7 +1,8 @@
-autoload -Uz peco-src peco-history peco-aws-credentials
+autoload -Uz peco-src peco-history peco-aws-credentials peco-z-search
 zle -N peco-src
 zle -N peco-history
 zle -N peco-aws-credentials
+zle -N peco-z-search
 
 bindkey ";5C" forward-word
 bindkey ";5D" backward-word
@@ -9,3 +10,4 @@ bindkey "\e[3~" delete-char
 bindkey '^]' peco-src
 bindkey '^r' peco-history
 bindkey '^w' peco-aws-credentials
+bindkey '^f' peco-z-search
