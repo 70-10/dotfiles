40af68b02bee59edb085d777ca5254e5ae9255df
70_10 <7010.dev@gmail.com>
2016-12-18 19:52:01 +0900

    [del] alias

--- .zsh.d/1_alias.sh
+++ .zsh.d/1_alias.sh
@@ -7,8 +7,3 @@ alias trje='trans ja:en'
 alias trej='trans en:ja'
 
 alias ssh-configs='cat ~/.ssh/config| grep "Host " | sed -e "s/Host //g"'
-
-# docker
-alias d-rmall='docker rm -v $(docker ps -aq -f status=exited)'
-
-alias pcd='cd $(find . -maxdepth 1 -type d | peco)'
