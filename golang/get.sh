c1a98fee3dfba87184e125809c7326d50e2c6975
70_10 <7010.dev@gmail.com>
2017-07-26 11:08:42 +0900

    [mod] add dep, delete glide

--- golang/get.sh
+++ golang/get.sh
@@ -15,8 +15,8 @@ error() {
 packages=(
   github.com/alecthomas/gometalinter
   github.com/b4b4r07/gist
+  github.com/golang/dep/cmd/dep
   github.com/golang/lint/golint
-  github.com/Masterminds/glide
   github.com/mattn/memo
   github.com/mattn/qq/cmd/qq
   github.com/motemen/gore
