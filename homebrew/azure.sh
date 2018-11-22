23f2880fa161fe295acdf0bb5386bb7fc91be42b
70_10 <7010.dev@gmail.com>
2018-11-22 13:51:18 +0900

    [add] homebrew/azure.sh

---
+++ homebrew/azure.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# .Net SDK
+brew cask install dotnet-sdk
+
+# Azure Functions Tools
+brew tap azure/functions
+brew install azure-functions-core-tools
+
+# Other
+brew cask install azure-data-studio
