e1910bc8dd4f5628c3f9b68a8adbcab0b912c072
70_10 <7010.dev@gmail.com>
2019-04-12 18:29:26 +0900

    [mod] go 1.12.4

--- anyenv/install.sh
+++ anyenv/install.sh
@@ -36,7 +36,7 @@ do
 done
 
 
-GOVERSION=1.12.3
+GOVERSION=1.12.4
 info "===> goenv install $GOVERSION"
 if [ -d $HOME/.anyenv/envs/goenv/versions/$GOVERSION ]; then
   info "$GOVERSION is already installed"
