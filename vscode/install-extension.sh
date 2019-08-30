59ea49250c627c466bdc6fb0af5a20041ee630b4
70_10 <7010.dev@gmail.com>
2019-08-30 17:51:12 +0900

    [mod] SettingsSync only VSCode

--- vscode/install-extension.sh
+++ vscode/install-extension.sh
@@ -13,22 +13,7 @@ error() {
 }
 
 extensions=(
-  christian-kohler.npm-intellisense
-  christian-kohler.path-intellisense
-  codezombiech.gitignore
-  dbaeumer.vscode-eslint
-  EditorConfig.EditorConfig
-  eg2.vscode-npm-script
-  esbenp.prettier-vscode
-  gluons.vscode-atom-javascript-snippet
-  ms-vscode.atom-keybindings
-  ms-vscode.go
-  octref.vetur
-  PeterJausovec.vscode-docker
-  PKief.material-icon-theme
-  robinbentley.sass-indented
-  WakaTime.vscode-wakatime
-  wayou.vscode-todo-highlight
+  shan.code-settings-sync
 )
 
 for ext in ${extensions[@]}
