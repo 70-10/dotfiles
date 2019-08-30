e96519dc5cd5767927c1f4e173f25b385e4e6f54
70_10 <7010.dev@gmail.com>
2019-08-30 17:39:10 +0900

    [add] homebrew/tools.sh

---
+++ homebrew/tools.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+info() {
+    echo -e "\033[35m$@\033[m"
+}
+
+warn() {
+    echo -e "\033[33m$@\033[m"
+}
+
+error() {
+    echo -e "\033[31m$@\033[m"
+}
+
+if test ! $(which brew); then
+    info "===> Installing homebrew"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+packages=(
+  jmeter
+)
+
+info "===> Install packages"
+brew install ${packages[@]}
+
+cask_packages=(
+  postman
+)
+
+info "===> Install Cask packages"
+brew cask install ${cask_packages[@]}
+
+info "===> Clean up"
+brew cleanup
