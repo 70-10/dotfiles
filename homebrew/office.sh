36f702e94ef255075bc6f311f0249e1afabea0be
70_10 <7010.dev@gmail.com>
2019-12-13 12:48:15 +0900

    [add] homebrew/office

---
+++ homebrew/office.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+info() {
+    echo -e "\033[35m$@\033[m"
+}
+
+warn() {
+    echo -e "\033[33m$@\033[m"
+}
+
+error() {
+    echo -e "\033[31m$@\033[m"
+}
+
+if test ! $(which brew); then
+    info "===> Installing homebrew"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+cask_packages=(
+  microsoft-excel
+  microsoft-teams
+  microsoft-powerpoint
+  microsoft-word
+  onedrive
+)
+
+
+info "===> Install Office packages"
+brew cask install ${cask_packages[@]}
+
+info "===> Clean up"
+brew cleanup
