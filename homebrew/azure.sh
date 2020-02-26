ffd9a753cec9fb99daa8e1aff0351bc40c48a5a4
70_10 <7010.dev@gmail.com>
2020-02-26 09:45:09 +0900

    [mod] dotnet ef

--- homebrew/azure.sh
+++ homebrew/azure.sh
@@ -7,6 +7,8 @@ brew install azure-cli
 brew cask install dotnet-sdk
 brew tap isen-ng/dotnet-sdk-versions
 
+dotnet tool install --global dotnet-ef --version 3.1.1
+
 # Azure Functions Tools
 brew tap azure/functions
 brew install azure-functions-core-tools
