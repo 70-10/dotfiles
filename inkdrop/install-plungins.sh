e855f1bdb02dcc118dd0d09f715c39f9938d5d34
70_10 <7010.dev@gmail.com>
2021-09-27 15:03:33 +0900

    Add plugins in inkdrop

--- inkdrop/install-plungins.sh
+++ inkdrop/install-plungins.sh
@@ -1,3 +1,10 @@
 #/bin/bash
 
-ipm install colorfi-for-japanese-preview markdown-emoji material-dark-mod-syntax vibrant-dark-ui
+ipm install \
+    colorfi-for-japanese-preview \
+    code-title \
+    markdown-emoji \
+    material-dark-mod-syntax \
+    simple-note-list \
+    table-editor \
+    vibrant-dark-ui
