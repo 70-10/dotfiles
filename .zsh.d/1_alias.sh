e97ae61f65a83d1a5cdb5332f874d4493ce5ef58
70_10 <7010.dev@gmail.com>
2023-01-06 22:45:47 +0900

    Modify zplug plugin

--- .zsh.d/1_alias.sh
+++ .zsh.d/1_alias.sh
@@ -1,13 +1,10 @@
 alias cdu='cd-gitroot'
-alias ka='k -a'
 alias mkdirp='mkdir -p'
 alias reload='source ~/.zshrc'
 
 alias ssh-configs='cat ~/.ssh/config| grep "Host " | sed -e "s/Host //g"'
 
 alias g='git'
-
-alias cl='richpager -s monokai'
 
 alias chrome='/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
 alias vim="nvim"
--- .zsh.d/zplug.sh
+++ .zsh.d/zplug.sh
@@ -6,20 +6,8 @@ zplug "zsh-users/zsh-completions"
 zplug "zsh-users/zsh-syntax-highlighting", defer:1
 zplug "zsh-users/zsh-history-substring-search"
 zplug "themes/minimal", from:oh-my-zsh, as:theme
-zplug "supercrabtree/k"
-zplug "b4b4r07/http_code"
 zplug "paulirish/git-open", as:command
-zplug "paulirish/git-recent", as:command
-zplug "jhawthorn/fzy", as:command, hook-build:'make'
 zplug "b4b4r07/git-br", as:command, use:'git-br'
-zplug "b4b4r07/richpager", as:command, use:"richpager"
-
-zplug "gchaincl/httplab", as:command, from:gh-r
-zplug "asciimoo/wuzz", as:command, from:gh-r
-
-zplug "takaaki-kasai/git-foresta", as:command
-zplug "lukechilds/zsh-better-npm-completion", defer:1
-zplug "b4b4r07/enhancd", use:init.sh
 
 if ! zplug check --verbose; then
     printf "Install? [y/N]: "
