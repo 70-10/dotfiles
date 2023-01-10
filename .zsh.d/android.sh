947e5c6d52239fe6dc0ae9b08774cf148166f0ac
70_10 <7010.dev@gmail.com>
2023-01-10 23:05:12 +0900

    Delete deno and android path

--- .zsh.d/android.sh
+++
@@ -1,4 +0,0 @@
-export ANDROID_HOME=$HOME/Library/Android/sdk
-export ANDROID_NDK_HOME=$ANDROID_HOME/ndk-bundle
-export PATH=$PATH:$ANDROID_HOME/platform-tools
-export PATH=$PATH:$ANDROID_HOME/tools
--- .zsh.d/deno.sh
+++
@@ -1,1 +0,0 @@
-export PATH="${HOME}/.deno/bin:$PATH"
