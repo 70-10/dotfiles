b3cb14ea7e352acd658738e6b000e5662488389a
70_10 <7010.dev@gmail.com>
2025-01-14 20:53:27 +0900

    Update ioup

--- zsh/defer/ioup.zsh
+++ zsh/defer/ioup.zsh
@@ -1,15 +1,9 @@
 ioup() {
-  asdf install nodejs lts;
-  asdf global nodejs lts;
+  asdf install nodejs latest;
+  asdf global nodejs latest;
   npm cache verify;
   npm i -g \
     clipboard-cli \
-    corepack \
-    eslint \
-    fkill-cli \
-    np \
     npm-check \
-    prettier \
-    textlint \
     trash-cli;
 }
